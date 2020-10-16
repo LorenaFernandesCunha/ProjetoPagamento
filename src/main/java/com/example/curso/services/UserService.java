ecc2d755197791184128e4f7ba29abecf1b9ff3c
Lorena Fernandes Cunha <lfernacu@everis.com>
2020-10-16 11:50:16 -0300

    Atualizacao e tratamento de excecoces

--- src/main/java/com/example/curso/services/UserService.java
+++ src/main/java/com/example/curso/services/UserService.java
@@ -1,13 +1,14 @@
 package com.example.curso.services;
 
 import java.util.List;
+import  java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.curso.entities.User;
 import com.example.curso.repositories.UserRepository;
-import  java.util.Optional;
+import com.example.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
 
@@ -22,10 +23,30 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		//return obj.get();
+		// Trocar para:
+		//orElseThrow tenta realizar o get. Caso nao encontre o objeto reakiza exception
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
+	
 }
